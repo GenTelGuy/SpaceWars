3a2e6229ac0c7501fb241de59c80a5b264788461
Alex Medwid <alex.medwid@gmail.com>
2015-02-14 14:33:35 -0800

    Got the basic server code working, needs a lot of changes but it works.

---
+++ src/clientCode/Player.java
@@ -0,0 +1,15 @@
+package clientCode;
+import java.io.Serializable;
+
+public class Player implements Serializable{
+   private static final long serialVersionUID = 1L;
+   int x;
+   int y;
+   int id;
+   
+   public Player(int x, int y, int id){
+      this.x = x;
+      this.y = y;
+      this.id = id;
+   }
+}
