5357004f757b71e10dbd542b81c5c9fd88159c84
Alex Medwid <alex.medwid@gmail.com>
2015-02-14 20:11:29 -0800

    Added code related to angle calculation to the GameMath class.

--- src/clientCode/GameMath.java
+++ src/clientCode/GameMath.java
@@ -1,6 +1,33 @@
 package clientCode;
 
+
 public class GameMath {//This class contains STATIC mathematical functions relevant to game development.
+	
+	public static float generateShotXVel( float playerX, float playerY, float mouseX, float mouseY, float magnitude ){
+		//This algorithm uses similar triangles to determine the appropriate x velocity for the projectile
+		
+		float hypotenuse = (float) (Math.pow(mouseX - playerX, 2) + Math.pow(mouseY - playerY, 2));
+		float ratio = magnitude / hypotenuse;
+		float xMag = ratio * (mouseX - playerX);
+		
+		return xMag;
+		
+		
+		
+	}
+	
+	public static float generateShotYVel( float playerX, float playerY, float mouseX, float mouseY, float magnitude ){
+		//This algorithm uses similar triangles to determine the appropriate y velocity for the projectile. No /0 errors
+		
+		float hypotenuse = (float) (Math.pow(mouseX - playerX, 2) + Math.pow(mouseY - playerY, 2));
+		float ratio = magnitude / hypotenuse;
+		float yMag = ratio * (mouseY - playerY);
+		
+		return yMag;
+		
+		
+		
+	}
 	
 
 }
